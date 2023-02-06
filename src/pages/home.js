791251e0c8ef28c670e3813b577aa3f5e43cb21d
sajid-munawar <sajidmunawar666@gmail.com>
2023-02-07 01:10:41 +0500

    Add button to navigate to register page

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { Button } from "../components";
+
+export const Home = () => {
+  return (
+    <>
+      <a href="/register">
+        <Button className="btn-filled">Register</Button>
+      </a>
+    </>
+  );
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes as Router } from "react-router-dom";
 import { Navigations } from "../config";
-import { Login, Register } from "../pages";
+import { Home, Login, Register } from "../pages";
 
 export function Routes() {
   return (
     <Router>
       <Route path={Navigations.LOGIN} element={<Login />} />
       <Route path={Navigations.Register} element={<Register />} />
+      <Route path='/' element={<Home />} />
     </Router>
   );
 }
