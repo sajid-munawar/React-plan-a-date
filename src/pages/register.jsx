00c2aff9b1eda1d2894b2c840c5eef0b634f85b1
sajid-munawar <sajidmunawar666@gmail.com>
2023-02-06 14:47:28 +0500

    Setup markup for register page

---
+++ src/pages/register.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Register = () => {
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-container">
+        <div className="logo-holder">
+          <img src="" alt="logo" />
+        </div>
+        <form className="auth-form">
+          <h1>Create An Account.</h1>
+          <p>Please enter below details to get registered.</p>
+          <div className="split-input">
+            <input type="text" />
+            <input type="text" />
+          </div>
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <div>
+            <input type="text" />
+            <p>Accept Terms & Conditions</p>
+          </div>
+          <button>Register</button>
+          <hr />
+          <p>Already have an account?</p>
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
